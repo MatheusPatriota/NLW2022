1d0a8e8ea0c3db8ed17e908adad9d48078db2e36
MatheusPatriota <mpatriota55@gmail.com>
2022-05-03 22:32:51 -0300

    Fix: Headless UI added to project, normal popup changed to popover as it implements ARIA concepts natively.

--- src/components/Widget/index.tsx
+++ src/components/Widget/index.tsx
@@ -1,24 +1,20 @@
 import { ChatTeardropDots } from "phosphor-react";
-import { useState } from "react";
-export function Widget() {
-  const [isWidgetOpen, setIsWidgetOpen] = useState(false);
+import { Popover } from "@headlessui/react";
 
-  //create function to swicth toggle visibility
-  function toggleWidgetVisibility() {
-    setIsWidgetOpen(!isWidgetOpen);
-  }
+// Estamos usando o Popover pois ele aplica os conceitos de acessibilidade
+// esse conceitos de acessibilidade são padronziados pela ARIA
+// O ARIA é um conjunto de atributos especiais para a acessibilidade
+
+export function Widget() {
   return (
-    <div className="absolute bottom-5 right-5">
-      {isWidgetOpen && <p>Hello World</p>}
-      <button
-        className="bg-brand-500 rounded-full px-3 h-12 text-white flex items-center group"
-        onClick={toggleWidgetVisibility}
-      >
+    <Popover className="absolute bottom-5 right-5">
+      <Popover.Panel>Hello World</Popover.Panel>
+      <Popover.Button className="bg-brand-500 rounded-full px-3 h-12 text-white flex items-center group">
         <ChatTeardropDots className="w-6 h-6" />
         <span className="max-w-0 overflow-hidden group-hover:max-w-xs transition-all duration-700 ease-linear">
           <span className="pl-2">Feedback</span>
         </span>
-      </button>
-    </div>
+      </Popover.Button>
+    </Popover>
   );
 }
