769279d4f69d0a6bebc8a58efd2de064c1d3d686
MatheusPatriota <mpatriota55@gmail.com>
2022-05-03 22:22:19 -0300

    start: Project initialized, feedback button created.

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import { Widget } from './components/Widget'
+
+export function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+   <div >
+     <Widget/>
+   </div>
+  )
+}
+
